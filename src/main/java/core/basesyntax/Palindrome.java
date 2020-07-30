e5724dbc13b93439a68dcc5a76dc996686bcc7d4
romanHreits <romagreic@gmail.com>
2020-07-30 09:43:54 +0300

    fixed checkstyle ERRORS

--- src/main/java/core/basesyntax/Palindrome.java
+++ src/main/java/core/basesyntax/Palindrome.java
@@ -21,7 +21,8 @@ public class Palindrome {
      * <p>Результат: true</p>
      */
     public boolean isPalindrome(String text) {
-        StringBuilder stringBuilder = new StringBuilder().append(text.replaceAll("\\W", "")).reverse();
+        StringBuilder stringBuilder = new StringBuilder()
+                .append(text.replaceAll("\\W", "")).reverse();
         return text.replaceAll("\\W", "").equalsIgnoreCase(stringBuilder.toString());
     }
 }
